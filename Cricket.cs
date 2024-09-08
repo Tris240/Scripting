ca05b61af2dd1e10031fa4491d2a7664340f9ae3
Tris240 <tristan.hildebrand@outlook.com>
2024-09-08 10:12:35 +0300

    Add files via upload

---
+++ Cricket.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cricket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       //Output to the console crickets
+       print("Crickets"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
